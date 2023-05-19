5da612b19568cf21024b2f24643a1a214557c4ac
ilariacaglio <ilaria.caglio@issgreppi.it>
2023-05-19 13:34:05 +0200

    File Upload

---
+++ easyNetAPI/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace easyNetAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        { }
+
+
+    }
+}
---
+++ easyNetAPI/easyNetModels/Comments.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace easyNetAPI.Models
+{
+    public class Comments
+    {
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public string Comment { get; set; }
+        public List<Like> Likes { get; set; }
+        public List<Comments> Answers { get; set; }
+    }
+
+}
