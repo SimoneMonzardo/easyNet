9b0a1118ebb0fbbf58fdc1952fca396228f3f82c
ilariacaglio <ilaria.caglio@issgreppi.it>
2023-05-19 13:22:10 +0200

    Model classes added

---
+++ easyNetAPI/easyNetModels/Like.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace easyNetModels
+{
+    public class Like
+    {
+        public int UserId { get; set; }
+    }
+}
