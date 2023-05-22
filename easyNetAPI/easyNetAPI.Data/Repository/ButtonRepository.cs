1e2e12e5cce6fbc444c237150dce73570c60a170
simone <simone.monzardo@issgreppi.it>
2023-05-22 09:54:37 +0200

    added IRepository interfaces TRepository classes IUnitOfWork interface UnitOfWork class and fixed models classes

---
+++ easyNetAPI/easyNetAPI.Data/Repository/ButtonRepository.cs
@@ -0,0 +1,23 @@
+﻿using easyNetAPI.Data.Repository.IRepository;
+using easyNetAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace easyNetAPI.Data.Repository
+{
+    public class ButtonRepository : Repository<Button>, IButtonRepository
+    {
+        public ButtonRepository(MongoDBService db) : base(db)
+        {
+
+        }
+
+        public void Update(Button button)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
