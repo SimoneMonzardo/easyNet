e43d6e923288e6163d22aac1b6c6a18088461e78
Filippo Ferrario <f.filippo49@gmail.com>
2023-05-24 09:53:18 +0200

    Component: Post Header

---
+++ components/PostHeader.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="flex flex-row justify-between align-middle mx-2">
+    <div class="flex flex-row gap-3">
+      <img class="rounded-full h-8 w-8 my-auto" :src="profilePictureUrl" alt="profile"/>
+      <div class="px-1">
+        <a :href="profileUrl">
+          <h3 class="text-xl font-semibold md:text-2xl text-gray-700">{{ userName }}</h3>
+        </a>
+        <small class="text-gray-400 text-xs font-md mr-2 dark:bg-gray-700 dark:text-gray-400">
+          {{ elapsedTime }} ago
+        </small>
+      </div>
+    </div>
+
+    <h2 class="my-auto text-2xl font-semibold leading-none tracking-tight text-gray-700 md:text-4xl dark:text-white">{{ postName }}</h2>
+
+    <div>
+      <button data-popover-target="actions-popover" data-popover-placement="left" data-popover-trigger="click" type="button">
+        <EllipsisVerticalIcon class="h-8 w-8 text-gray-700 dark:text-white" />
+      </button>
+      <div data-popover id="actions-popover" role="tooltip" class="absolute z-10 invisible inline-block w-64 text-sm text-gray-500 transition-opacity duration-300 bg-white border border-gray-200 rounded-lg shadow-sm opacity-0 dark:text-gray-400 dark:border-gray-600 dark:bg-gray-800">
+        <div class="w-48 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+          <a href="#" class="block w-full px-4 py-2 text-white bg-blue-700 border-b border-gray-200 rounded-t-lg cursor-pointer dark:bg-gray-800 dark:border-gray-600">
+            Report
+          </a>
+          <a href="#" class="block w-full px-4 py-2 border-b border-gray-200 cursor-pointer hover:bg-gray-100 hover:text-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-500 dark:focus:text-white">
+            Share
+          </a>
+          <a :href="postUrl" class="block w-full px-4 py-2 border-b border-gray-200 cursor-pointer hover:bg-gray-100 hover:text-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-500 dark:focus:text-white">
+            Copy link
+          </a>
+          <a :href="profileUrl" class="block w-full px-4 py-2 rounded-b-lg cursor-pointer hover:bg-gray-100 hover:text-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-500 dark:focus:text-white">
+            About this account
+          </a>
+        </div>
+        <div data-popper-arrow></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import { EllipsisVerticalIcon } from "@heroicons/vue/24/outline";
+
+  export default {
+    props: {
+        userName: String,
+        postName: String,
+        elapsedTime: String,
+        profilePicture: String,
+        profileId: String,
+        postId: String
+    },
+    components: {
+        EllipsisVerticalIcon
+    },
+    computed: {
+      profilePictureUrl() {
+        return this.profilePicture;
+      },
+      profileUrl() {
+        // TODO: Use the rigth link
+        return `./users/${this.profileId}`;
+      },
+      postUrl() {
+        // TODO: Use the right link
+        return `./posts/${this.postId}`;
+      }
+    }
+  }
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <PostHeader elapsedTime="69 min" userName="utent" postName="NOME POST" profilePicture="https://upload.wikimedia.org/wikipedia/commons/8/89/%D0%A1%D0%BD%D0%B5%D0%B3_2015.jpg" profileId="prova"/>
     <PostsFeedSection title="Home" :loading="loading">
       <head>
         <title>Home / Easynet</title>
