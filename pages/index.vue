e00bc65d9e14254a2907bb20cc37f2904e94392a
DAVIDE RICCARDO HOFFER 5IB_STUDENTI <davide.hoffer@issgreppi.it>
2023-05-24 11:29:59 +0200

    pages

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <PostsFeedSection title="Home" :loading="loading">
+      <head>
+        <title>Home / Easynet</title>
+      </head>
+
+      <div class="border-b">
+        <PostsListFeed :posts="homePosts" />
+      </div>
+    </PostsFeedSection>
+  </div>
+</template>
+<script setup>
+const loading = ref(true);
+const homePosts = ref([
+  {
+    username: "Marione",
+    content: "questo è un esempio di contenuto di un post",
+  },
+  {
+    username: "Giorgio",
+    content: "questo è un altro esempio di contenuto di un post",
+  },
+]);
+</script>
---
+++ pages/settings.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <SettingSelector />
+  </div>
+</template>
+<script setup></script>
