199eb365ba4bd166b40b26bc25e196744277101d
DAVIDE RICCARDO HOFFER 5IB_STUDENTI <davide.hoffer@issgreppi.it>
2023-05-25 11:31:32 +0200

    pupup

--- components/LikeCommentsButtons.vue
+++ components/LikeCommentsButtons.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="inline-flex rounded-md" role="group">
-    <button type="button" class="shadow-inner shadow-neutral-600 font-semibold gap-1 inline-flex items-center px-3 py-1 text-sm font-medium text-black bg-neutral-400 border-r border-r-gray-900 rounded-l-full hover:bg-neutral-500 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:bg-gray-700">
-      <HeartIcon class="h-6 w-6" />
+  <div class="flex flex-row">
+    <button class="shadow-inner shadow-neutral-600 rounded-l-full bg-neutral-400 flex flex-row gap-x-2 pl-4 pr-2 py-1 font-semibold align-middle">
+      <HeartIcon class="h-6 w-6 text-black" />
       {{ likes }}
     </button>
-    <button type="button" class="shadow-inner shadow-neutral-600 font-semibold gap-1 inline-flex items-center px-3 py-1 text-sm font-medium text-black bg-neutral-400 border-l border-l-gray-900 rounded-r-full hover:bg-neutral-500 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:bg-gray-700">
-      <ChatBubbleBottomCenterTextIcon class="h-6 w-6" />
+    <button class="border-l-2 border-l-neutral-700 shadow-inner shadow-neutral-600 rounded-r-full bg-neutral-400 flex flex-row gap-x-2 pl-2 pr-4 py-1 font-semibold">
+      <ChatBubbleBottomCenterTextIcon class="h-6 w-6 text-black" />
       {{ comments }}
     </button>
   </div>
