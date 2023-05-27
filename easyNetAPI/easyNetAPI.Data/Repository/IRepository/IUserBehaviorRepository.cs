08132ddad1021d11d36bd2a2b5f6c74abdd7661f
ilariacaglio <ilaria.caglio@issgreppi.it>
2023-05-27 16:55:13 +0200

    UserBehaviorRepository fixed

--- easyNetAPI/easyNetAPI.Data/Repository/IRepository/IUserBehaviorRepository.cs
+++ easyNetAPI/easyNetAPI.Data/Repository/IRepository/IUserBehaviorRepository.cs
@@ -11,16 +11,16 @@ namespace easyNetAPI.Data.Repository.IRepository
     {
         public Task<List<UserBehavior>> GetAllAsync();
         public Task<UserBehavior?> GetFirstOrDefault(string userId);
-        public Task AddAsync(UserBehavior user);
+        public Task<bool> AddAsync(UserBehavior user);
         /// <summary>
         /// updates one or more UserBehavior objects from DB.
         /// param:"users" dictionary of all the users to update key: userId value: updated user
         /// </summary>
         /// <param name="users"></param>
         /// <returns></returns>
-        public Task UpdateManyAsync(Dictionary<string, UserBehavior> users);
-        public Task UpdateOneAsync(string userId, UserBehavior user);
-        public Task RemoveAsync(string userId);
-        public Task RemoveUserActivityAsync(UserBehavior userToDelete, IUnitOfWork _unitOfWork);
+        public Task<bool> UpdateManyAsync(Dictionary<string, UserBehavior> users);
+        public Task<bool> UpdateOneAsync(string userId, UserBehavior user);
+        public Task<bool> RemoveAsync(string userId);
+        public Task<bool> RemoveUserActivityAsync(UserBehavior userToDelete, IUnitOfWork _unitOfWork);
     }
 }
