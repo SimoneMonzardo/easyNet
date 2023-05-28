97907ffadc2f64ebad9ea259b57724498d3c5826
Filippo Ferrario <f.filippo49@gmail.com>
2023-05-28 18:54:03 +0200

    Create UploadImagePopup component

--- pages/settings/profile.vue
+++ pages/settings/profile.vue
@@ -21,13 +21,19 @@
             <h3 class="text-lg font-normal text-gray-500 dark:text-gray-400">Sei certo di voler cancellare l'account?</h3>
             <div class="my-5">
               <!-- TODO: Use username -->
-              <label for="confirm-delete-text"
-                class="text-left block mb-1 text-sm font-medium text-gray-900 dark:text-white">Scrivi <span
-                  class="text-red-600 font-semibold">elimina.utente</span> per confermare</label>
-              <input v-on:keyup="validateConfirmDelete()" type="text" id="confirm-delete-text"
+              <label for="confirm-delete-text" class="text-left block mb-1 text-sm font-medium text-gray-900 dark:text-white">
+                Scrivi <span class="text-red-600 font-semibold">elimina.utente</span> per confermare
+              </label>
+              <input @input="confirmDeleteText = $event.target.value" :value="confirmDeleteText" type="text"
                 class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
             </div>
-            <button disabled data-modal-hide="confirm-delete-modal" type="button" id="confirm-delete-modal-confirm" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-semibold rounded-xl text-sm inline-flex items-center px-5 py-2.5 text-center mr-2 disabled:cursor-not-allowed disabled:bg-red-400">
+            <!-- TODO: Use username -->
+            <button
+              id="confirm-delete-modal-confirm"
+              :disabled="confirmDeleteText !== `elimina.${'utente'}`" 
+              data-modal-hide="confirm-delete-modal" 
+              type="button" 
+              class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-semibold rounded-xl text-sm inline-flex items-center px-5 py-2.5 text-center mr-2 disabled:cursor-not-allowed disabled:bg-red-400">
               Sì, elimina
             </button>
             <button data-modal-hide="confirm-delete-modal" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-xl border border-gray-200 text-sm font-semibold px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">
@@ -39,32 +45,7 @@
     </div>
 
     <!-- Change Image Modal -->
-    <div id="change-image-modal" tabindex="-1" aria-hidden="true"
-      class="bg-gray-900 bg-opacity-50 fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-full max-h-full">
-      <div class="relative w-full max-w-lg max-h-full">
-        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-          <button type="button"
-            class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white"
-            data-modal-hide="change-image-modal">
-            <XMarkIcon class="h-6 w-6 text-gray-500 dark:text-gray-400" />
-            <span class="sr-only">Chiudi</span>
-          </button>
-          <div class="px-6 py-6 lg:px-8">
-            <h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">Cambia la tua immagine profilo</h3>
-            <form class="space-y-6" action="#" enctype="multipart/form-data" method="post">
-              <UploadFile />
-
-              <button data-modal-hide="change-image-modal" type="submit" class="text-white bg-green-600 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-semibold rounded-xl text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
-                Salva le modifiche
-              </button>
-              <button data-modal-hide="change-image-modal" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-xl border border-gray-200 text-sm font-semibold px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">
-                Annulla
-              </button>
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
+    <UploadImagePopup />
 
     <!-- Login & Register Modals -->
     <RegisterPopup />
@@ -86,7 +67,7 @@
         <!-- Loaded Successfully -->
         <img v-else src="" class="w-32 h-32 lg:h-40 lg:w-40 rounded-full border border-gray-600 dark:border-gray-100" />
 
-        <button type="button" data-modal-target="change-image-modal" data-modal-toggle="change-image-modal" :disabled="pending ? true : false"
+        <button type="button" data-modal-target="upload-image-modal" data-modal-toggle="upload-image-modal" :disabled="pending ? true : false"
           class="w-[calc(100%-2rem)] text-black bg-gray-300 hover:bg-gray-400 focus:outline-none focus:ring-4 focus:ring-blue-300 font-semibold rounded-xl text-lg py-2 text-center mr-2 mb-2 dark:text-white dark:bg-gray-700 dark:hover:bg-gray-800 dark:focus:ring-blue-800 disabled:bg-gray-100 disabled:cursor-not-allowed">Modifica</button>
       </div>
       <div class="w-full md:w-1/2 2xl:w-1/3 py-3">
@@ -187,24 +168,16 @@ import { ExclamationCircleIcon } from "@heroicons/vue/24/outline";
 import { XMarkIcon } from "@heroicons/vue/24/outline";
 
 export default {
+  data: () => ({
+    confirmDeleteText: ''
+  }),
   components: {
     ExclamationCircleIcon,
     XMarkIcon
   },
   methods: {
-    validateConfirmDelete() {
-      const value = document.getElementById('confirm-delete-text').value;
-      const confirmButton = document.getElementById('confirm-delete-modal-confirm');
-
-      // TODO: Use username
-      if (value === `elimina.${'utente'}`) {
-        confirmButton.removeAttribute('disabled');
-      } else {
-        confirmButton.setAttribute('disabled', '');
-      }
-    },
     resetModal() {
-      document.getElementById('confirm-delete-text').value = '';
+      this.confirmDeleteText = '';
       document.getElementById('confirm-delete-modal-confirm').setAttribute('disabled', '');
     }
   }
