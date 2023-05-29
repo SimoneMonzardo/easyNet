92131f843f3af5057b59ccdc1046911f3a232a5b
Tommaso Toloni <tommaso.toloni@issgreppi.it>
2023-05-29 17:56:37 +0200

    bug fix 1

--- components/RegisterPopup.vue
+++ components/RegisterPopup.vue
@@ -108,8 +108,8 @@
 
                                 <!-- TODO: Add profile picture upload -->
 
-                                
-                                <button type="submit" @click="handleRegister()" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-xl text-sm px-5 py-2.5s p text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Crea Account</button>
+
+                                <!-- <button type="submit" @click="handleRegister()" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-xl text-sm px-5 py-2.5s p text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Crea Account</button> -->
                             </div>
                         </form>
                     </div>
