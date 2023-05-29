717b32681e54077e462a8fdc2fbe10ece9b5d9a5
Filippo Ferrario <f.filippo49@gmail.com>
2023-05-29 16:36:05 +0200

    Test Login

--- composables/useAuth.js
+++ composables/useAuth.js
@@ -1,139 +1,143 @@
-export default() => {
+export default () => {
 
-    //to do: mettere gli auth token negli header
+  //to do: mettere gli auth token negli header
 
-    const register = async ({email, username, name, surname, dateOfBirth, gender, profilePicture, phoneNumber, password, role})=>{
-        const { data, pending, error, refresh } = await useFetch('/api/auth/register', {
-            method : 'POST',
-            body: {
-                         email,
-                         username,
-                         name,
-                         surname,
-                         dateOfBirth,
-                         gender,
-                         profilePicture,
-                         phoneNumber,
-                         password,
-                         role
-            },
-            onRequest({ request, options }) {
-              // Set the request headers
+  const register = async ({ email, username, name, surname, dateOfBirth, gender, profilePicture, phoneNumber, password, role }) => {
+    const { data, pending, error, refresh } = await useFetch('/api/auth/register', {
+      method: 'POST',
+      body: {
+        email,
+        username,
+        name,
+        surname,
+        dateOfBirth,
+        gender,
+        profilePicture,
+        phoneNumber,
+        password,
+        role
+      },
+      onRequest({ request, options }) {
+        // Set the request headers
 
-            },
-            onRequestError({ request, options, error }) {
-              // Handle the request errors
-            },
-            onResponse({ request, response, options }) {
-              // Process the response data
-              localStorage.setItem('token', response._data.token)
-              return response
-            },
-            onResponseError({ request, response, options }) {
-              // Handle the response errors
-            }
-        })
-    }
+      },
+      onRequestError({ request, options, error }) {
+        // Handle the request errors
+      },
+      onResponse({ request, response, options }) {
+        // Process the response data
+        localStorage.setItem('token', response._data.token)
+        return response
+      },
+      onResponseError({ request, response, options }) {
+        // Handle the response errors
+      }
+    })
+  }
 
-    const login = async ({username, password })=>{
-        const { data, pending, error, refresh } = await useFetch('/api/auth/login', {
-            method : 'POST',
-            body: {
-                         username,
-                         password,
-            },
-            onRequest({ request, options }) {
-              // Set the request headers
-
-            },
-            onRequestError({ request, options, error }) {
-              // Handle the request errors
-            },
-            onResponse({ request, response, options }) {
-              // Process the response data
-              localStorage.setItem('token', response._data.token)
-              return response
-            },
-            onResponseError({ request, response, options }) {
-              // Handle the response errors
-            }
-        })
-    }
-
-    
-    const changePassword = async ({oldPassword, newPassword})=>{
-        const { data, pending, error, refresh } = await useFetch('/api/auth/changepassword', {
-            method : 'POST',
-            body: {
-                oldPassword,
-                newPassword
-            },
-            onRequest({ request, options }) {
-              // Set the request headers
-
-            },
-            onRequestError({ request, options, error }) {
-              // Handle the request errors
-            },
-            onResponse({ request, response, options }) {
-              // Process the response data
-              return response
-            },
-            onResponseError({ request, response, options }) {
-              // Handle the response errors
-            }
-        })
-    }
-
-        
-    const deleteUser = async ()=>{
-        const { data, pending, error, refresh } = await useFetch('/api/auth/deleteUser', {
-            method : 'POST',
-            body: {
-            },
-            onRequest({ request, options }) {
-              // Set the request headers
-
-            },
-            onRequestError({ request, options, error }) {
-              // Handle the request errors
-            },
-            onResponse({ request, response, options }) {
-              // Process the response data
-              return response
-            },
-            onResponseError({ request, response, options }) {
-              // Handle the response errors
-            }
-        })
-    }
-
-            
-    const getUserData = async ()=>{
-        const { data, pending, error, refresh } = await useFetch('/api/auth/getUserData', {
-            method : 'GET',
-            onRequest({ request, options }) {
+  const login = async (credentials) => {
+    const { data, pending, error, refresh } = await useFetch('https://localhost:44359/Auth/login', {
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      method: 'POST',
+      body: JSON.stringify(credentials),
+      onRequest({ request, options }) {
+        // Set the request headers
+      },
+      onRequestError({ request, options, error }) {
+        // Handle the request errors
+      },
+      onResponse({ request, response, options }) {
+        // Process the response data
+        localStorage.setItem('logged', true);
+        localStorage.setItem('username', response._data.username);
+        localStorage.setItem('email', response._data.email);
+        localStorage.setItem('profilePicture', response._data.profilePicture);
+        localStorage.setItem('token', response._data.token);
+        return response
+      },
+      onResponseError({ request, response, options }) {
+        // Handle the response errors
+      }
+    })
+  }
 
 
-            },
-            onRequestError({ request, options, error }) {
+  const changePassword = async ({ oldPassword, newPassword }) => {
+    const { data, pending, error, refresh } = await useFetch('/api/auth/changepassword', {
+      method: 'POST',
+      body: {
+        oldPassword,
+        newPassword
+      },
+      onRequest({ request, options }) {
+        // Set the request headers
 
-            },
-            onResponse({ request, response, options }) {
+      },
+      onRequestError({ request, options, error }) {
+        // Handle the request errors
+      },
+      onResponse({ request, response, options }) {
+        // Process the response data
+        return response;
+      },
+      onResponseError({ request, response, options }) {
+        // Handle the response errors
+        return response.error;
+      }
+    })
+  }
 
-              return response
-            },
-            onResponseError({ request, response, options }) {
 
-            }
-        })
-    }
+  const deleteUser = async () => {
+    const { data, pending, error, refresh } = await useFetch('/api/auth/deleteUser', {
+      method: 'POST',
+      body: {
+      },
+      onRequest({ request, options }) {
+        // Set the request headers
 
-        return{
-            register,
-            login,
-            changePassword,
-            deleteUser,
-            getUserData
-        }
+      },
+      onRequestError({ request, options, error }) {
+        // Handle the request errors
+      },
+      onResponse({ request, response, options }) {
+        // Process the response data
+        return response
+      },
+      onResponseError({ request, response, options }) {
+        // Handle the response errors
+      }
+    })
+  }
+
+
+  const getUserData = async () => {
+    const { data, pending, error, refresh } = await useFetch('/api/auth/getUserData', {
+      method: 'GET',
+      onRequest({ request, options }) {
+
+
+      },
+      onRequestError({ request, options, error }) {
+
+      },
+      onResponse({ request, response, options }) {
+
+        return response
+      },
+      onResponseError({ request, response, options }) {
+
+      }
+    })
+  }
+
+  return {
+    register,
+    login,
+    changePassword,
+    deleteUser,
+    getUserData
+  }
 }
