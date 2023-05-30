fc480a14b80fa10ea835d34d5d29225f79a992ca
ilariacaglio <ilaria.caglio@issgreppi.it>
2023-05-30 22:32:13 +0200

    Deploy changes

--- easyNetAPI/easyNetAPI/Program.cs
+++ easyNetAPI/easyNetAPI/Program.cs
@@ -133,11 +133,11 @@ builder.Services.AddAuthorization(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//
 
 app.UseAuthentication();
 
