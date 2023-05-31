0c3b0adaa1c6d087c51a51c467b35095fccd7e53
Filippo Ferrario <f.filippo49@gmail.com>
2023-05-31 09:52:17 +0200

    Add delete profile picture

--- components/UploadImagePopup.vue
+++ components/UploadImagePopup.vue
@@ -34,10 +34,20 @@
               </label>
             </div>
 
-            <button data-modal-hide="upload-image-modal" type="button"
-              class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-xl border border-gray-200 text-sm font-semibold px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">
-              Annulla
-            </button>
+            <div class="flex flex-row justify-between">
+              <button data-modal-hide="upload-image-modal" type="button"
+                class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-xl border border-gray-200 text-sm font-semibold px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">
+                Annulla
+              </button>
+              <button
+                v-if="showDelete"
+                type="button" 
+                data-modal-hide="danger-confirm-modal" 
+                class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-semibold rounded-xl text-sm inline-flex items-center px-5 py-2.5 text-center"
+                @click="$emit('delete')">
+                Elimina
+              </button>
+            </div>
           </div>
         </div>
       </div>
@@ -50,7 +60,10 @@ import { XMarkIcon } from "@heroicons/vue/24/outline";
 
 export default {
   name: 'UploadImagePopup',
-  emits: ['setImage'],
+  props: {
+    showDelete: false
+  },
+  emits: ['setImage', 'delete'],
   components: {
     XMarkIcon
   }
--- pages/settings/index.vue
+++ pages/settings/index.vue
@@ -44,7 +44,10 @@
     </div>
 
     <!-- Change Image Modal -->
-    <UploadImagePopup @setImage="updateImage" />
+    <UploadImagePopup 
+      @setImage="updateImage"
+      @delete="deleteImage" 
+      showDelete="true" />
 
     <!-- Login & Register Modals -->
     <RegisterPopup />
@@ -57,7 +60,7 @@
     <div class="justify-center flex flex-col sm:flex-row gap-4 mx-10 md:mx-auto mt-12">
       <div class="w-40 lg:w-48 flex flex-col items-center gap-2 lg:pt-4 mx-auto sm:mx-0">
         <!-- Loading -->
-        <div v-if="pending" class="flex items-center justify-center w-32 h-32 lg:h-40 lg:w-40 bg-gray-300 rounded-full dark:bg-gray-700">
+        <div v-if="pending || user.profilePicture === ''" class="flex items-center justify-center w-32 h-32 lg:h-40 lg:w-40 bg-gray-300 rounded-full dark:bg-gray-700">
           <svg class="w-12 h-12 text-gray-200" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" fill="currentColor" viewBox="0 0 640 512">
             <path d="M480 80C480 35.82 515.8 0 560 0C604.2 0 640 35.82 640 80C640 124.2 604.2 160 560 160C515.8 160 480 124.2 480 80zM0 456.1C0 445.6 2.964 435.3 8.551 426.4L225.3 81.01C231.9 70.42 243.5 64 256 64C268.5 64 280.1 70.42 286.8 81.01L412.7 281.7L460.9 202.7C464.1 196.1 472.2 192 480 192C487.8 192 495 196.1 499.1 202.7L631.1 419.1C636.9 428.6 640 439.7 640 450.9C640 484.6 612.6 512 578.9 512H55.91C25.03 512 .0006 486.1 .0006 456.1L0 456.1z" />
           </svg>
@@ -264,6 +267,23 @@ async function saveChanges() {
     profilePicture: user.profilePicture
   };
 
+  const oldPicture = localStorage.getItem('backupProfilePicture');
+  if (oldPicture !== '' && user.profilePicture === '') {
+    await useFetch('https://progettoeasynet.azurewebsites.net/Auth/DeleteProfilePicture', {
+      method: 'DELETE',
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Authorization': ''
+      },
+      onRequest({ request, options }) {
+        options.headers['Authorization'] = `Bearer ${localStorage.getItem('token')}`;
+      },
+      onResponseError({response}) {
+        console.log(response);
+      }
+    });
+  }  
+
   await useFetch('https://progettoeasynet.azurewebsites.net/Auth/editUserData', {
     method: 'POST',
     headers: {
@@ -335,5 +355,14 @@ async function updateImage(images) {
   if (data._value !== null) {
     user.profilePicture = data._value;
   }
+}
+
+async function deleteImage() {
+  const options = {};
+  const uploadFileElement = document.getElementById('upload-image-modal');
+  const uploadFileModal = new Modal(uploadFileElement, options);
+  uploadFileModal.hide();
+
+  user.profilePicture = '';
 }
 </script>
