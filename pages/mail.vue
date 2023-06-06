717010b65d4cb43525cae4f0ff58e17ba095b309
Davide Hoffer <davide.hoffer@issgreppi.it>
2023-06-06 11:16:17 +0200

    mailpage

---
+++ pages/mail.vue
@@ -0,0 +1,18 @@
+<template>
+      <h3 class=" transition-colors mb-3 py-20 text-4xl text-gray-900 dark:text-white">Email confermata</h3>
+</template>
+
+<script setup>
+import { reactive } from 'vue';
+import { useRouter } from "vue-router";
+import useFormat from '~/composables/useFormat';
+import * as matter from 'gray-matter';
+
+</script>
+<script>
+export default {
+  head: {
+    title:"Mail"
+  }
+}
+</script>
