714e891bdcede2b18c037365592f11f7dca72e37
Filippo Ferrario <f.filippo49@gmail.com>
2023-06-12 16:10:08 +0200

    Fix Registration bug & Partial Fix Create post

--- components/CreateCompany.vue
+++ components/CreateCompany.vue
@@ -6,7 +6,7 @@
       <div class="relative bg-white rounded-lg shadow dark:bg-gray-700 flex flex-row">
         <div
           class="bg-gray-200 dark:bg-gray-800 flex-col content-center rounded-l-lg w-64 p-5 text-gray-900 justify-evenly hidden sm:flex">
-          <h6 class="mx-auto text-4xl font-semibold text-blue-500">easyNet</h6>
+          <h6 class="mx-auto text-4xl font-semibold text-violet-500">easyNet</h6>
           <img v-if="isDark" src="~/public/muznet-white.png" class="mt-3 h-15 rounded-full" alt="MuzNet Logo" />
           <img v-else-if="!isDark" src="~/public/muznet-black.png" class="mt-3 h-15 rounded-full" alt="MuzNet Logo" />
         </div>
@@ -30,10 +30,10 @@
               <div class="grid gap-4 mb-4 sm:grid-cols-2 sm:gap-6 sm:mb-5">
                 <div class="relative">
                   <input type="text" id="name" name="name"
-                    class="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border border-gray-300 appearance-none dark:text-white dark:border-gray-500 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                    class="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border border-gray-300 appearance-none dark:text-white dark:border-gray-500 dark:focus:border-violet-500 focus:outline-none focus:ring-0 focus:border-violet-600 peer"
                     placeholder=" " required />
                   <label for="name"
-                    class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-700 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1">Nome
+                    class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-700 px-2 peer-focus:px-2 peer-focus:text-violet-600 peer-focus:dark:text-violet-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1">Nome
                   </label>
                 </div>
               </div>
@@ -42,7 +42,7 @@
                   profilo azienda</label>
                 <input v-on:change="updateDocument($event.target.files, 'document1')"
                   class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
-                  aria-describedby="user_avatar_help" id="input_document1" type="file" name="img" accept="application/pdf"
+                  aria-describedby="user_avatar_help" id="input_document1" type="file" name="img" accept="image/png"
                   required>
                 <div class="mt-1 text-sm text-gray-500 dark:text-gray-300" id="user_avatar_help">È necessario inserire un
                   documento
@@ -62,15 +62,23 @@
               <fieldset>
                 <legend class="sr-only">Checkbox variants</legend>
                 <div class="flex items-center mb-4">
-                  <input id="checkbox-1" type="checkbox" value="" required
-                    class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                  <input 
+                    id="checkbox-1" 
+                    type="checkbox" 
+                    value="" 
+                    required 
+                    @click="() => { this.accepted = !this.accepted; }"
+                    class="w-4 h-4 text-violet-600 bg-gray-100 border-gray-300 rounded focus:ring-violet-500 dark:focus:ring-violet-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
                   <label for="checkbox-1" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Dichiaro di
                     accettare i Termini & Condizioni</label>
                 </div>
               </fieldset>
               <div class="flex flex-row-reverse">
-                <button type="submit" v-on:click="handleCreation()"
-                  class="p-1.5 text-white bg-blue-500 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-xl text-sm px-5 py-2.5s p text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                <button
+                  :disabled="!this.accepted" 
+                  type="submit" 
+                  v-on:click="handleCreation()"
+                  class="w-full text-white bg-violet-600 hover:bg-violet-700 focus:ring-4 focus:outline-none focus:ring-violet-300 font-medium rounded-xl text-sm px-5 py-2.5 text-center dark:bg-violet-700 dark:hover:bg-violet-700 dark:focus:ring-violet-800 disabled:bg-violet-300">
                   Crea azienda
                 </button>
               </div>
@@ -85,16 +93,27 @@
 <script>
 export default {
   name: "createCompany",
-  data: () => ({ formData: new FormData() }, { docsData: new FormData() }),
+  data: () => ({ 
+    accepted: false, 
+    docsData: new FormData() 
+  }),
   methods: {
     async api() {
+      var companyData = {
+        companyId: 0,
+        companyName: document.getElementById("name").value,
+        profilePicture: "",
+        bot: {},
+        documents: []
+      };
+
       await useFetch(`https://progettoeasynet.azurewebsites.net/Azienda/RequestToAddCompany`, {
         headers: {
           "Access-Control-Allow-Origin": "*",
           'Authorization': ""
         },
         method: "POST",
-        body: this.formData,
+        body: JSON.stringify(companyData),
         onRequest({ options }) {
           options.headers['Authorization'] = `Bearer ${sessionStorage.getItem('token')}`;
         },
@@ -109,7 +128,7 @@ export default {
       });
     },
     async postDocuments() {
-      const { data } = await useFetch('https://progettoeasynet.azurewebsites.net/Azienda/postDocuments', {
+      await useFetch('https://progettoeasynet.azurewebsites.net/Azienda/postDocuments', {
         method: 'POST',
         headers: {
           'Access-Control-Allow-Origin': '*',
@@ -120,24 +139,15 @@ export default {
           options.headers['Authorization'] = `Bearer ${sessionStorage.getItem('token')}`;
         }
       });
-      if (data._value !== null) {
-        user.profilePicture = data._value;
-        sessionStorage.setItem('document', data._value);
-      }
     },
-    async updateDocument(_document, name) {
+    updateDocument(_document, name) {
+      if (this.docsData.get(name) !== null) {
+        this.docsData.delete(name);
+      }
+
       this.docsData.append(name, _document[0]);
     },
     async handleCreation() {
-      var companyData = {
-        companyId: 0,
-        companyName: document.getElementById("name").value,
-        profilePicture: "",
-        bot: {},
-        documents: []
-      }
-      this.formData = ('data', JSON.stringify(companyData));
-
       await this.api();
       await this.postDocuments();
     }
